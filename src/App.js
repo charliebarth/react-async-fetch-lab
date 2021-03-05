57e771888977dd846bffe17c64615752d0a54932
github username <charliepbarth@gmail.com>
2021-03-04 19:37:29 -0500

    commit tree 78dd79acf0cc13f8da0a36d9fb3b0facc90a96f1

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
-// create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(data => data.json())
+    .then(data => console.log("test"))
+  }
+
+  render() {
+    return "Test"
+  }
+}
